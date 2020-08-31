11e3065e263d7d6900bb303ff576e77d185f6bc1
tendev_kh <60765259+tendevkh@users.noreply.github.com>
2020-08-31 19:11:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ class Bot {
         
         this.discordClient.on("message", (msg) => {this.processMessage(msg)});
         
-        this.discordClient.login(Config.discord_token);
+        this.discordClient.login(process.env.TOKEN);
     }
     
     initialize() {
